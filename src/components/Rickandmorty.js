2dcf240788a17f87828e742ce7b85b9622b32a59
katyabykova <ab221085bev@gmail.com>
2022-09-14 19:27:39 +0300

    hw1

--- src/components/Rickandmorty.js
+++ src/components/Rickandmorty.js
@@ -1,22 +1,31 @@
-import React from 'react';
-import {rickandmorty} from "../data";
+import React, {useState} from 'react';
 
 const Rickandmorty = () => {
+
+    let [users,setUsers] = useState([]);
+
+    fetch('https://rickandmortyapi.com/api/character')
+        .then(value=>value.json())
+        .then(value => {
+                setUsers(value.results);
+
+        })
     return (
         <div>
             <h3>2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього
                 апі.
                 Створити 6 персонажів</h3>
-            {rickandmorty.map(value =>
-                <div className={'rick'}>
+            {
+                users.slice(0,6).map(value =>
+                    <div className={'rick'}>
                     <p>ID: {value.id}</p>
                     <p>Name: {value.rickname}</p>
                     <p>Status: {value.status}</p>
                     <p>Spesies:{value.spesies}</p>
                     <p>Genger:{value.gender}</p>
-                    <img src={value.rickpick}/>
-                </div>
-            )}
+                    <img src={value.image}/>
+                </div>)
+            }
         </div>
     );
 };
